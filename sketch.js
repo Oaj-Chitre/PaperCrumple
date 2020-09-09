8306d500ff23301964db01c1248774dfcf33a0ee
Oaj-Chitre <69528513+Oaj-Chitre@users.noreply.github.com>
2020-09-08 22:56:54 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var ball;
+var ground;
 
 function preload()
 {
@@ -19,7 +20,13 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-	ball = new paper(100,390,20);
+	ball = new Paper(100,390,40);
+	
+	ground = new Ground(400,690,800,15);
+
+	box1 = new Box(600,655,100,10);
+	box2 = new Box(650,625,10,70);
+	box3 = new Box(550,625,10,70);
 
 	Engine.run(engine);
 	
@@ -28,13 +35,28 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
   background(0);
-  
+  Engine.update(engine);
+
+
   ball.display();
+
+  ground.display();
+
+  box1.display();
+  box2.display();
+  box3.display();
 
   drawSprites();
  
+}
+
+function keyPressed()
+{
+	if (keyCode === UP_ARROW)
+	{
+		Matter.Body.applyForce(ball.body, ball.body.position,{x:240,y:-200});
+	}
 }
 
 
